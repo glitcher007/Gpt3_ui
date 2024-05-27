05241902703e55b7f3903ec76eed5e6c5e2c1718
Kundan tiwari <112776999+glitcher007@users.noreply.github.com>
2024-05-27 22:07:38 +0530

    Add files via upload

---
+++ src/components/brand/imports.js
@@ -0,0 +1,18 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
+
+/*
+this file is made to import all the brand images that will ve used
+*/
+
